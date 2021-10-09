b02230e938300efbe097844f79ff8b01cdabf851
Marc Beninca <marc@rwx.work>
2021-10-10 01:27:34 +0200

    loop

--- package.py
+++ package.py
@@ -7,17 +7,13 @@ SEPARATOR = f'{os.linesep}{os.linesep}'
 
 class Package:
     def __init__(self, package, files):
-        p = package.decode(CHARSET).strip()
-        f = files.decode(CHARSET).strip()
-        text = f'{p}{SEPARATOR}{f}'
-        for item in text.split(SEPARATOR):
-            line, *lines = item.split(os.linesep)
-            key = line.split(KEY)[1].lower()
-            if len(lines) == 1:
-                value = lines[0]
-            else:
-                value = lines
-            setattr(self, key, value)
-            print()
-            print(key)
-            print(value)
+        for binary in [package, files]:
+            text = binary.decode(CHARSET).strip()
+            for item in text.split(SEPARATOR):
+                line, *lines = item.split(os.linesep)
+                key = line.split(KEY)[1].lower()
+                if len(lines) == 1:
+                    value = lines[0]
+                else:
+                    value = lines
+                setattr(self, key, value)
